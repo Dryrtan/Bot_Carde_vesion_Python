0dffad7bd080dceaeb23be897e95d22dd6d65e69
Dryrtan <kleidimar@gmail.com>
2021-01-31 17:02:08 -0300

    Versão 1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+1° -> Deve instalar o irssi com: sudo apt update && sudo apt install irssi -y
+
+2° -> Inicie o progrma com o comando 'irssi' para que seja criando a pasta .irssi
+
+3° -> Após isso deve mover o arquivo "config" para ~/.irssi/config
+
+4° -> Para que bot funcione normalmente os irssi deve rodar em backgroud por isso use o screen para isso, o google vai te ajudar com isso
+
+5° -> Mude o que precisa ser mudado dentro de bot.py, como diretorio_principal, arquivo_filtrado, bot_token e bot_chatID
+
+6° -> Na teoria tá tudo pronto, use o screen ou o nohup para deixar o bot sempre ativo, de preferencia user um VPC a AWS vai te ajudar com isso com 1 ano gratis de uso.
+
+Fique a vontade para comtribuir.
+
+ATENÇÃO: PARA A CRIAÇÃO DESTE SCRIPT EM NENHUM MOMENTO LEIS FORAM VIOLADAS, O SCRIPT APENAS MOSTRA INFORMAÇÕES OBTIDAS NO IRC DA CHKNET, O USO DAS INFORMAÇÕES OBTIDAS PELO MESMO E DE TOTAL RESPONSABILIDADE DO USUARIO.
---
+++ bot_principal.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+#!/bin/python3
+
+import time
+import requests
+import sys
+import os
+import time
+
+diretorio_principal = '/home/fulando/irclogs/Chknet/#brazil.log' # O diretório de usuario e local de logs exemplo: "/home/fulando/irclogs/Chknet/#brazil.log"
+arquivo_filtrado = '/home/fulando/irclogs/Chknet/data.txt' #Local dos dados gravados de preferencia coloque o mesmo local do diretorio_principal
+
+while True:
+        os.system('egrep "<%" ' + diretorio_principal + ' > ' + arquivo_filtrado)
+        arq = open(arquivo_filtrado, 'r')
+
+        def telegram_bot_sendtext(bot_message):
+                bot_token = '1491360647:AAH_7g_PSRwuLLQu8nT_VztvolnSoe7lIu8'
+                bot_chatID = '@Dryrtan_channel_infos'
+                send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+                response = requests.get(send_text)
+
+                return response.json()
+
+        qtn_linhas = len(arq.readline())
+        contador_de_linhas = 0
+
+        with open(arquivo_filtrado) as file:
+            for line in file:
+                #Seleciona as BINs
+                if 'BIN:' in line and '<%' in line.split(' ')[1] and line.split(' ')[4] == '»':
+                    bin = line.split(' ')
+                    telegram_bot_sendtext(bin[2]+' '+bin[3]+'\nBandeira: '+bin[5]+'\nTipo: '+bin[7]+'\nLimite: '+bin[9]+'\nPais:'+bin[11]+"\n#bin")
+
+                #Seleciona os dados fulls
+                if 'RESULTADOs' in line and '<%' in line and 'https:' in line.split(' ')[6]:
+                    telegram_bot_sendtext('Dados Full\n'+'Link: '+line.split(' ')[6])
+
+                #Seleciona todos as CCs aprovadas
+                if 'APPROVED!s' in line and '<%' in line:
+                    telegram_bot_sendtext('CC FULL\n'+line.split(' ')[5]+'\n'+line.split(' ')[6]+'\n'+line.split(' ')[7]+'\nBandeira: '+line.split(' ')[17].split('|')[0]+'\n')
+
+
+        #os.system("echo '' > "+diretorio_principal)
+
+
+        arq.close()
